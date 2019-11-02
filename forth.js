5c361ba121ce3658a64bfc517597fdfe978132bd
Christopher Martinez <crmartinez239@protonmail.com>
2019-11-02 08:53:45 -0400

    Rename numberStack to dataStack (ANS Forth)

--- forth.js
+++ forth.js
@@ -81,155 +81,155 @@ class InvalidWord extends Word {
 class Fvm {
 
     constructor() {
-        this.numberStack = [];
+        this.dataStack = [];
         this.status = StatusTypes.OK;
         this.output = '';
         
         this.words = {
             '.s': function(self) {
                 let stackString = self.stackToString();
-                let stackCount = self.numberStack.length;
+                let stackCount = self.dataStack.length;
                 self.output = `<${stackCount}> ${stackString}`;
             },
     
             'drop': function(self){
                 self.checkStackUnderflow(0);
-                self.numberStack.pop();
+                self.dataStack.pop();
             },
     
             'dup': function(self) {
                 self.checkStackUnderflow(0);
-                let w = self.numberStack[self.numberStack.length - 1];
-                self.numberStack.push(w);      
+                let w = self.dataStack[self.dataStack.length - 1];
+                self.dataStack.push(w);      
             },
     
             'nip': function(self) {
                 self.checkStackUnderflow(1);
-                let w2 = self.numberStack.pop();
-                self.numberStack.pop(); // w1
-                self.numberStack.push(w2);
+                let w2 = self.dataStack.pop();
+                self.dataStack.pop(); // w1
+                self.dataStack.push(w2);
             },
 
             'over': function(self) {
                 self.checkStackUnderflow(1);
-                let w1 = self.numberStack[self.numberStack.length - 2];
-                self.numberStack.push(w1);
+                let w1 = self.dataStack[self.dataStack.length - 2];
+                self.dataStack.push(w1);
             },
 
             'tuck': function(self) {
                 self.checkStackUnderflow(1);
-                let w2 = self.numberStack.pop();
-                let w1 = self.numberStack.pop();
-                self.numberStack.push(w2, w1, w2);
+                let w2 = self.dataStack.pop();
+                let w1 = self.dataStack.pop();
+                self.dataStack.push(w2, w1, w2);
             },
 
             'swap': function(self) {
                 self.checkStackUnderflow(1);
-                let w2 = self.numberStack.pop();
-                let w1 = self.numberStack.pop();
-                self.numberStack.push(w2, w1);
+                let w2 = self.dataStack.pop();
+                let w1 = self.dataStack.pop();
+                self.dataStack.push(w2, w1);
             },
 
             'pick': function(self) {
                 self.checkStackUnderflow(0);
-                let topNumber = self.numberStack.pop();
+                let topNumber = self.dataStack.pop();
                 self.checkStackUnderflow(topNumber);
-                let pickedNumber = self.numberStack[(self.numberStack.length - 1) - topNumber];
-                self.numberStack.push(pickedNumber);
+                let pickedNumber = self.dataStack[(self.dataStack.length - 1) - topNumber];
+                self.dataStack.push(pickedNumber);
             },
 
             'rot': function(self) {
                 self.checkStackUnderflow(2);
-                let w3 = self.numberStack.pop();
-                let w2 = self.numberStack.pop();
-                let w1 = self.numberStack.pop();
-                self.numberStack.push(w2, w3, w1);
+                let w3 = self.dataStack.pop();
+                let w2 = self.dataStack.pop();
+                let w1 = self.dataStack.pop();
+                self.dataStack.push(w2, w3, w1);
             },
 
             '-rot': function(self) {
                 self.checkStackUnderflow(2);
-                let w3 = self.numberStack.pop();
-                let w2 = self.numberStack.pop();
-                let w1 = self.numberStack.pop();
-                self.numberStack.push(w3, w1, w2);
+                let w3 = self.dataStack.pop();
+                let w2 = self.dataStack.pop();
+                let w1 = self.dataStack.pop();
+                self.dataStack.push(w3, w1, w2);
             },
 
             '?dup': function(self) {
                 self.checkStackUnderflow(0);
-                let topNumber = self.numberStack[self.numberStack.length - 1];
+                let topNumber = self.dataStack[self.dataStack.length - 1];
                 if (topNumber) {
-                    self.numberStack.push(topNumber);
+                    self.dataStack.push(topNumber);
                 }
             },
 
             'roll': function(self) {
                 self.checkStackUnderflow(0);
-                let rollAmount = self.numberStack.pop();
+                let rollAmount = self.dataStack.pop();
                 self.checkStackUnderflow(rollAmount);
                 
             },
 
             '2drop': function(self) {
                 self.checkStackUnderflow(1);
-                self.numberStack.pop(); // w2
-                self.numberStack.pop(); // w1
+                self.dataStack.pop(); // w2
+                self.dataStack.pop(); // w1
             },
 
             '2nip': function(self) {
                 self.checkStackUnderflow(3);
-                let w4 = self.numberStack.pop();
-                let w3 = self.numberStack.pop();
-                self.numberStack.pop(); // w2
-                self.numberStack.pop(); // w1
-                self.numberStack.push(w3, w4);
+                let w4 = self.dataStack.pop();
+                let w3 = self.dataStack.pop();
+                self.dataStack.pop(); // w2
+                self.dataStack.pop(); // w1
+                self.dataStack.push(w3, w4);
             },
 
             '2dup': function(self) {
                 self.checkStackUnderflow(1);
-                let w2 = self.numberStack.pop();
-                let w1 = self.numberStack.pop();
-                self.numberStack.push(w1, w2, w1, w2);
+                let w2 = self.dataStack.pop();
+                let w1 = self.dataStack.pop();
+                self.dataStack.push(w1, w2, w1, w2);
             },
 
             '2over': function(self) {
                 self.checkStackUnderflow(3);
-                let len = self.numberStack.length
-                let w1 = self.numberStack[len - 4];
-                let w2 = self.numberStack[len - 3];
-                self.numberStack.push(w1, w2);
+                let len = self.dataStack.length
+                let w1 = self.dataStack[len - 4];
+                let w2 = self.dataStack[len - 3];
+                self.dataStack.push(w1, w2);
             },
 
             '2tuck': function(self) {
                 self.checkStackUnderflow(3);
-                let w4 = self.numberStack.pop();
-                let w3 = self.numberStack.pop();
-                let w2 = self.numberStack.pop();
-                let w1 = self.numberStack.pop();
-                self.numberStack.push (
+                let w4 = self.dataStack.pop();
+                let w3 = self.dataStack.pop();
+                let w2 = self.dataStack.pop();
+                let w1 = self.dataStack.pop();
+                self.dataStack.push (
                     w3, w4, w1, w2, w3, w4
                 );
             },
 
             '2swap': function(self) {
                 self.checkStackUnderflow(3);
-                let w4 = self.numberStack.pop();
-                let w3 = self.numberStack.pop();
-                let w2 = self.numberStack.pop();
-                let w1 = self.numberStack.pop();
-                self.numberStack.push (
+                let w4 = self.dataStack.pop();
+                let w3 = self.dataStack.pop();
+                let w2 = self.dataStack.pop();
+                let w1 = self.dataStack.pop();
+                self.dataStack.push (
                     w3, w4, w1, w2
                 );
             },
 
             '2rot': function(self) {
                 self.checkStackUnderflow(5);
-                let w6 = self.numberStack.pop();
-                let w5 = self.numberStack.pop();
-                let w4 = self.numberStack.pop();
-                let w3 = self.numberStack.pop();
-                let w2 = self.numberStack.pop();
-                let w1 = self.numberStack.pop();
-                self.numberStack.push (
+                let w6 = self.dataStack.pop();
+                let w5 = self.dataStack.pop();
+                let w4 = self.dataStack.pop();
+                let w3 = self.dataStack.pop();
+                let w2 = self.dataStack.pop();
+                let w1 = self.dataStack.pop();
+                self.dataStack.push (
                     w3, w4, w5, w6, w1, w2
                 );
             }
@@ -252,7 +252,7 @@ class Fvm {
             }
 
             if (w instanceof NumberWord) {
-                this.numberStack.push(w.value);
+                this.dataStack.push(w.value);
                 continue;
             }
 
@@ -344,26 +344,26 @@ class Fvm {
 
     attemptMathOperation(type) {
         // empty stack error
-        if (this.numberStack.length == 0) {
+        if (this.dataStack.length == 0) {
             this.status = StatusTypes.ERROR;
             throw new StackError(ErrorMessages.STACK_UNDERFLOW);
         }
         
-        if (this.numberStack.length == 1) {
-            this.numberStack.pop();
+        if (this.dataStack.length == 1) {
+            this.dataStack.pop();
             return;
         }
 
         // perform operation on stack
-        const topVar = this.numberStack.pop();
-        const bottomVar = this.numberStack.pop();
+        const topVar = this.dataStack.pop();
+        const bottomVar = this.dataStack.pop();
         const newVar = this.operate(topVar, bottomVar, type);
 
-        this.numberStack.push(newVar);
+        this.dataStack.push(newVar);
     }
 
     checkStackUnderflow(equalToOrLessThan) {
-        if (this.numberStack.length <= equalToOrLessThan) {
+        if (this.dataStack.length <= equalToOrLessThan) {
             this.status = StatusTypes.ERROR;
             throw new StackError(ErrorMessages.STACK_UNDERFLOW);
         }
@@ -371,7 +371,7 @@ class Fvm {
 
     stackToString() {
         let ret = '';
-        this.numberStack.forEach(n => {
+        this.dataStack.forEach(n => {
             ret += n + ' '
         })
         return ret.trim();
